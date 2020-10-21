e09cee3b64a283ab0f5df92087c91ca4b8536d29
siumingdev <siumingdev@gmail.com>
2020-10-21 16:53:41 +0800

    update data preprocessing pipeline

--- kedro_test/kaggle-titanic/src/kaggle_titanic/pipelines/data_engineering/nodes.py
+++ kedro_test/kaggle-titanic/src/kaggle_titanic/pipelines/data_engineering/nodes.py
@@ -1,9 +1,75 @@
 import pandas as pd
 
 
+def combine_kaggle_dataset(train_df: pd.DataFrame, test_df: pd.DataFrame) -> pd.DataFrame:
+    train_df['is_test_set'] = False
+    test_df['is_test_set'] = True
+    return pd.concat([train_df, test_df])
+
+
 def get_filled_age(df: pd.DataFrame) -> pd.Series:
     return df.groupby(['Sex', 'Pclass'])['Age'].apply(lambda x: x.fillna(x.median()))
 
 
 def get_filled_embarked(df: pd.DataFrame) -> pd.Series:
     return df['Embarked'].fillna('S')
+
+
+def get_filled_fare(df: pd.DataFrame) -> pd.Series:
+    med_fare = df.groupby(['Pclass', 'Parch', 'SibSp']).Fare.median()[3][0][0]
+    return df['Fare'].fillna(med_fare)
+
+
+def get_deck_from_cabin(df: pd.DataFrame) -> pd.Series:
+    df['Deck'] = df['Cabin'].apply(lambda s: s[0] if pd.notnull(s) else 'M')
+    df.loc[df[df['Deck'] == 'T'].index, 'Deck'] = 'A'
+    df['Deck'] = df['Deck'].replace(['A', 'B', 'C'], 'ABC')
+    df['Deck'] = df['Deck'].replace(['D', 'E'], 'DE')
+    df['Deck'] = df['Deck'].replace(['F', 'G'], 'FG')
+    return df['Deck']
+
+
+def get_title_from_name(df: pd.DataFrame) -> pd.Series:
+    return df['Name'].str.split(', ', expand=True)[1].str.split('.', expand=True)[0]
+
+
+def get_surname_from_name(df: pd.DataFrame) -> pd.Series:
+    def extract_surname(name: str) -> str:
+        import string
+
+        if '(' in name:
+            name_no_bracket = name.split('(')[0]
+        else:
+            name_no_bracket = name
+
+        family = name_no_bracket.split(',')[0]
+        # title = name_no_bracket.split(',')[1].strip().split(' ')[0]
+
+        for c in string.punctuation:
+            family = family.replace(c, '').strip()
+
+        return family
+
+    return df['Name'].map(extract_surname)
+
+
+def get_master_table(
+        raw_df: pd.DataFrame,
+        age: pd.Series,
+        embarked: pd.Series,
+        fare: pd.Series,
+        deck: pd.Series,
+        title: pd.Series,
+        surname: pd.Series) -> pd.DataFrame:
+    df = raw_df
+    df['Age'] = age
+    df['Embarked'] = embarked
+    df['Fare'] = fare
+    df['Deck'] = deck
+    df['Title'] = title
+    df['Surname'] = surname
+    return df
+
+
+def identity(df):
+    return df
--- kedro_test/kaggle-titanic/src/kaggle_titanic/pipelines/data_engineering/pipeline.py
+++ kedro_test/kaggle-titanic/src/kaggle_titanic/pipelines/data_engineering/pipeline.py
@@ -1,22 +1,72 @@
 from kedro.pipeline import Pipeline, node
 
-from .nodes import get_filled_age, get_filled_embarked
+from .nodes import *
 
 
 def create_pipeline(**kwargs):
     return Pipeline(
         [
             node(
+                func=combine_kaggle_dataset,
+                inputs=["train_raw", "test_raw"],
+                outputs="combined_raw",
+                name="Combine raw data"
+            ),
+            node(
                 func=get_filled_age,
-                inputs="train",
-                outputs="train_filled_age",
-                name="train_filled_age"
+                inputs="combined_raw",
+                outputs="preprocessed_age",
+                name="clean \"Age\""
             ),
             node(
                 func=get_filled_embarked,
-                inputs="train",
-                outputs="train_filled_embarked",
-                name="train_filled_embarked"
+                inputs="combined_raw",
+                outputs="preprocessed_embarked",
+                name="clean \"Embarked\""
             ),
+            node(
+                func=get_filled_fare,
+                inputs="combined_raw",
+                outputs="preprocessed_fare",
+                name="clean \"Fare\""
+            ),
+            node(
+                func=get_deck_from_cabin,
+                inputs="combined_raw",
+                outputs="preprocessed_deck",
+                name="calculate \"Deck\" from \"Cabin\""
+            ),
+            node(
+                func=get_title_from_name,
+                inputs="combined_raw",
+                outputs="preprocessed_title",
+                name="calculate \"Title\" from \"Name\""
+            ),
+            node(
+                func=get_surname_from_name,
+                inputs="combined_raw",
+                outputs="preprocessed_surname",
+                name="calculate \"Surname\" from \"Name\""
+            ),
+            node(
+                func=get_master_table,
+                inputs=[
+                    "combined_raw",
+                    "preprocessed_age",
+                    "preprocessed_embarked",
+                    "preprocessed_fare",
+                    "preprocessed_deck",
+                    "preprocessed_title",
+                    "preprocessed_surname"
+                ],
+                outputs="master_table",
+                name="Combine cleaned data"
+            ),
+            node(
+                func=identity,
+                inputs="train_1_raw",
+                outputs="train_2_raw",
+                name="Dummy"
+            )
         ]
     )
